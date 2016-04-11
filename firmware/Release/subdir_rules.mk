027b22cbdd9343799603d368b5583e8214b4d237
Albert Jansen <ajansen314@gmail.com>
2016-04-11 14:19:53 -0400

    Added CodeStartBranch.asm in attempt to solve boot to flash error

--- firmware/Release/subdir_rules.mk
+++ firmware/Release/subdir_rules.mk
@@ -3,6 +3,13 @@
 ################################################################################
 
 # Each subdirectory must supply rules for building sources it contributes
+F2806x_CodeStartBranch.obj: C:/ti/controlSUITE/device_support/f2806x/v150/F2806x_common/source/F2806x_CodeStartBranch.asm $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.6/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --cla_support=cla0 --vcu_support=vcu0 -O2 --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.6/include" --include_path="C:/ti/controlSUITE/device_support/f2806x/v150/F2806x_headers/include" --include_path="C:/ti/controlSUITE/device_support/f2806x/v150/MWare" --include_path="C:/ti/controlSUITE/device_support/f2806x/v150/F2806x_common/include" --define=_INLINE --define=ccs_c2k --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="F2806x_CodeStartBranch.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 F2806x_DefaultIsr.obj: C:/ti/controlSUITE/device_support/f2806x/v150/F2806x_common/source/F2806x_DefaultIsr.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: C2000 Compiler'
